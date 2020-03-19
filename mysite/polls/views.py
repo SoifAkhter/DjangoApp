1bfb5efde0ff0beb7b2d8f0d221d492d43f7cd12
Soif Akhter <soifakhter@gmail.com>
2020-03-19 23:26:24 +0530

    update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import sys
+print(sys.path)
 
 # Create your views here.
 def index(request):
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+import sys
 
-urlpatterns = [
+'''urlpatterns = [
 	path('polls/', include('polls.urls')),
 	path('admin/', admin.site.urls),
-]
+]'''
+print(sys.path)
